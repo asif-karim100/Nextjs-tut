aba84428fdb4bb5bffe103b1237e97a54a8b5342
asifkarim <asif.karim100@gmail.com>
2023-11-25 20:09:47 +0530

    Final touch to About US page Navigating LINKS

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
- const about = () => {
+function aboutUs() {
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
   return (
-    <div> this is about page</div>
-  )
-}
+    <div>
+      <h1>Hello from About Us Page</h1>
+      {details.map((item) => (
+        // one Way of building Link
+        // <Link key={item.id} href={`/about/${item.id}`}>
+        <Link 
+          key={item.id}
 
-export default about;
+          href={{
+            pathname: "/about/[id]",
+            query: { id: item.id },
+          }}
+        >
+          <h1>{item.name}</h1>
+        </Link>
+      ))}
+    <style jsx>{`
+        h1 {
+          color: black;
+          margin-right: 10px; // Adjust the margin to your preference
+        }
+      `}</style>
+    </div>
+  );
+}
+export default aboutUs;
