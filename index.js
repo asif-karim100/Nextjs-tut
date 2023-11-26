d8e1b538771321786642f36eb81d314b3706a64b
Asif Karim <113389378+asif-karim100@users.noreply.github.com>
2023-11-26 21:56:03 +0530

    Prerendering the powers of Next JS
    getstaticpath getserverpath

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import MeetupList from "../components/meetups/MeetupList";
+const Dummy_MeetUps = [
+    {
+       id: 'm1',
+       title: ' A first meetup',
+       image:  'https://upload.wikimedia.org/wikipedia/commons/4/45/RUS-2016-SPB-Monument_to_Nicholas_I_of_Russia.jpg',
+       address: 'somewhere in earth',
+       description: 'this is first meetup'
+    },
+    {
+        id: 'm2',
+        title: ' A second meetup',
+        image:  'https://upload.wikimedia.org/wikipedia/commons/4/45/RUS-2016-SPB-Monument_to_Nicholas_I_of_Russia.jpg',
+        address: 'somewhere in second earth',
+        description: 'this is second meetup'
+     }
+]
 
-function NewMeetUpPage() {
 
-    function addMeetupHandler (enteredMeetupData){
-        console.log(enteredMeetupData);
 
+
+
+const HomePage = (props) => {
+   
+  return (
+
+  <MeetupList meetups={props.meetups}/>
+
+  )
+  
+}
+// export async function getServerSideProps(context){
+//     const req = context.req;
+//     const res = context.res;
+//     //fetch data from an API
+//     return {
+//         props: {
+//             meetups:Dummy_MeetUps
+//         }
+//     }
+
+// }
+export async function getStaticProps(){
+    return{
+        props:{
+            meetups:Dummy_MeetUps
+        },
+        revalidate:1
     }
-    return <NewMeetupForm  onAddMeetup={addMeetupHandler}/>
-
-
 }
 
-export default NewMeetUpPage;
+export default HomePage;
