c2364ba11c05b231769d04169c349578a1a8eb39
Asif Karim <113389378+asif-karim100@users.noreply.github.com>
2023-11-27 00:27:05 +0530

    post and get request from mongodb
    Time to become a Full Stack Developer

--- index.js
+++ index.js
@@ -1,53 +1,28 @@
 
-import MeetupList from "../components/meetups/MeetupList";
-const Dummy_MeetUps = [
-    {
-       id: 'm1',
-       title: ' A first meetup',
-       image:  'https://upload.wikimedia.org/wikipedia/commons/4/45/RUS-2016-SPB-Monument_to_Nicholas_I_of_Russia.jpg',
-       address: 'somewhere in earth',
-       description: 'this is first meetup'
-    },
-    {
-        id: 'm2',
-        title: ' A second meetup',
-        image:  'https://upload.wikimedia.org/wikipedia/commons/4/45/RUS-2016-SPB-Monument_to_Nicholas_I_of_Russia.jpg',
-        address: 'somewhere in second earth',
-        description: 'this is second meetup'
-     }
-]
+import { useRouter } from "next/router";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
+function NewMeetUpPage() {
+    const router = useRouter();
 
+    async function addMeetupHandler (enteredMeetupData){
+       const response=await fetch('/api/new-meetup',{
+        method:'POST',
+        body:JSON.stringify(enteredMeetupData),
+        headers:{
+            'Content-Type':'application/json'
+                }
+       });
+       const data = await response.json();
 
+       console.log(data);
 
+       router.push('/');
 
-const HomePage = (props) => {
-   
-  return (
-
-  <MeetupList meetups={props.meetups}/>
-
-  )
-  
-}
-// export async function getServerSideProps(context){
-//     const req = context.req;
-//     const res = context.res;
-//     //fetch data from an API
-//     return {
-//         props: {
-//             meetups:Dummy_MeetUps
-//         }
-//     }
-
-// }
-export async function getStaticProps(){
-    return{
-        props:{
-            meetups:Dummy_MeetUps
-        },
-        revalidate:1
     }
+    return <NewMeetupForm  onAddMeetup={addMeetupHandler}/>
+
+
 }
 
-export default HomePage;
+export default NewMeetUpPage;
